12e36e3506c5c85630047c2e320b7ceb42ca77b9
kyumin1227 <kyumin12271227@gmail.com>
2025-08-28 23:54:17 +0900

    Feat: Go 학습용 예제 코드 추가
    - ch22: 고루틴

---
+++ ch22/ex1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func PrintHangul() {
+	hanguls := []rune{'가', '나', '다', '라', '마', '바', '사'}
+	for _, v := range hanguls {
+		time.Sleep(300 * time.Millisecond)
+		fmt.Printf("%c ", v)
+	}
+}
+
+func PrintNumber() {
+	for i := 1; i <= 5; i++ {
+		time.Sleep(400 * time.Millisecond)
+		fmt.Printf("%d ", i)
+	}
+}
+
+func main() {
+	go PrintHangul()
+	go PrintNumber()
+
+	time.Sleep(3 * time.Second)	// Sleep을 하지 않을 경우 서브 고루틴이 돌고 있어도 메인 함수는 끝나기에 프로그램 종료
+}
