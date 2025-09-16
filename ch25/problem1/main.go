6e7d8478b9fdda687bdfbaf00fc1164be291953d
kyumin1227 <kyumin12271227@gmail.com>
2025-09-16 23:24:11 +0900

    Feat: Go 학습용 예제 코드 추가
    - ch25: 단어 검색 프로그램

---
+++ ch25/problem1/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type LineInfo struct {
+	lineNo int
+	line   string
+}
+
+type FindInfo struct {
+	filename string
+	lines    []LineInfo
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("3개의 실행 인수가 필요합니다. ex) ex25.3 word pattern")
+		return
+	}
+
+	word := os.Args[1]
+	pattern := os.Args[2]
+	findInfos := []FindInfo{}
+
+	files, _ := GetFileList(pattern)
+
+	for _, path := range files {
+		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
+	}
+	for _, findInfo := range findInfos {
+		fmt.Println(findInfo.filename)
+		fmt.Println("---------------------")
+		for _, lineInfo := range findInfo.lines {
+			fmt.Println("\t", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println("---------------------")
+		fmt.Println()
+	}
+}
+
+func GetFileList(pattern string) ([]string, error) {
+	filelist := []string{}
+	err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() {
+			matched, _ := filepath.Match(pattern, info.Name())
+			if matched {
+				filelist = append(filelist, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return []string{}, err
+	}
+	return filelist, nil
+}
+
+func FindWordInAllFiles(word, path string) []FindInfo {
+	findInfos := []FindInfo{}
+
+	filelist, err := GetFileList(path)
+	if err != nil {
+		fmt.Println("파일 경로가 잘못되었습니다. err:", err, "path:", path)
+		return findInfos
+	}
+
+	ch := make(chan FindInfo)
+	cnt := len(filelist)
+	recvCnt := 0
+
+	for _, filename := range filelist {
+		go FindWordInFile(word, filename, ch)
+	}
+
+	for findInfo := range ch {
+		findInfos = append(findInfos, findInfo)
+		recvCnt++
+		if recvCnt == cnt {
+			break
+		}
+	}
+	return findInfos
+}
+
+func FindWordInFile(word, filename string, ch chan FindInfo) {
+	findInfo := FindInfo{filename, []LineInfo{}}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("파일을 찾을 수 없습니다. ", filename)
+		ch <- findInfo
+		return
+	}
+	defer file.Close()
+
+	lineNo := 1
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, word) {
+			findInfo.lines = append(findInfo.lines, LineInfo{lineNo, line})
+		}
+		lineNo++
+	}
+	ch <- findInfo
+}
