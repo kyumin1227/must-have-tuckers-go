d43832c12ba25cf2cd810050bf44c2be56c20261
kyumin1227 <kyumin12271227@gmail.com>
2025-09-18 02:02:36 +0900

    Feat: Go 학습용 예제 코드 추가
    - ch30: gnet & grpc로 채팅 앱 만들기

---
+++ ch30/chat/server/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/panjf2000/gnet/v2"
+)
+
+type chatServer struct {
+	gnet.BuiltinEventEngine
+
+	cliMap sync.Map
+}
+
+func (cs *chatServer) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
+	log.Printf("client connected. address:%s", c.RemoteAddr().String())
+
+	cs.cliMap.Store(c, true)
+	return nil, gnet.None
+}
+
+func (cs *chatServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
+	log.Printf("client disconnected. address:%s", c.RemoteAddr().String())
+
+	if _, ok := cs.cliMap.LoadAndDelete(c); ok {
+		log.Printf("connection removed")
+	}
+	return gnet.None
+}
+
+func (cs *chatServer) OnBoot(eng gnet.Engine) gnet.Action {
+	log.Printf("chat server is listening\n")
+	return gnet.None
+}
+
+func (cs *chatServer) OnTraffic(c gnet.Conn) gnet.Action {
+	buf, _ := c.Next(-1)
+
+	cs.cliMap.Range(func(key, value any) bool {
+		if conn, ok := key.(gnet.Conn); ok {
+			conn.AsyncWrite(buf, nil)
+		}
+		return true
+	})
+	return gnet.None
+}
+
+func main() {
+	var port int
+	var multicore bool
+
+	flag.IntVar(&port, "port", 9000, "--port 9000")
+	flag.BoolVar(&multicore, "multicore", false, "--multicore true")
+	flag.Parse()
+
+	chat := &chatServer{}
+	log.Fatal(gnet.Run(chat, fmt.Sprintf("tcp://:%d", port), gnet.WithMulticore(multicore)))
+}
